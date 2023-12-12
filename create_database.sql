5a6af4c4590d4e77e7427785a26aee42644bd020
Matteo Vallar <matteo.vallar@gmail.com>
2023-12-12 15:26:23 +0100

    Creating also other tables

--- create_database.sql
+++ create_database.sql
@@ -33,3 +33,24 @@ IGNORE 1 ROWS
 ALTER TABLE consumption RENAME COLUMN `Cigarette consumption per smoker per day (IHME, GHDx (2012))` TO consumption_day;
 ALTER TABLE consumption RENAME COLUMN `Consumption per smoker per day upper bound (IHME, GHDx (2012))` TO consumption_day_upper;
 ALTER TABLE consumption RENAME COLUMN `Consumption per smoker per day lower bound (IHME, GHDx (2012))` TO consumption_day_lower;
+
+/*Create a table with the same fields as the data you have*/
+/* Entity,Code,Year,"Share of total deaths that are from all causes attributed to smoking, in both sexes aged age-standardized"*/
+CREATE TABLE IF NOT EXISTS total_deaths(
+id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+Entity VARCHAR(20),
+Code VARCHAR(3),
+Year YEAR,
+share_deaths DOUBLE
+);
+
+LOAD DATA LOCAL INFILE './data/share-deaths-smoking.csv'
+INTO TABLE total_deaths
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS
+(`Entity`, `Code`, `Year`, 
+  @share_deaths)
+SET
+ `Share of total deaths that are from all causes attributed to smoking, in both sexes aged age-standardized` = NULLIF(@share_deaths, '');
