86c02659d6db136ca34b43e548041f63b244f4aa
Matteo Vallar <matteo.vallar@gmail.com>
2023-12-12 17:31:50 +0100

    now creating database with the names I want and not the ones in the csv file

--- create_database.sql
+++ create_database.sql
@@ -44,13 +44,47 @@ Year YEAR,
 share_deaths DOUBLE
 );
 
+/*Fill a table with different fields as the data you have*/
 LOAD DATA LOCAL INFILE './data/share-deaths-smoking.csv'
 INTO TABLE total_deaths
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS
-(`Entity`, `Code`, `Year`, 
-  @share_deaths)
+(`Entity`, `Code`, `Year`, @csv_share_deaths)
 SET
- `Share of total deaths that are from all causes attributed to smoking, in both sexes aged age-standardized` = NULLIF(@share_deaths, '');
+  share_deaths=@csv_share_deaths;
+
+/*Create age_deaths table*/
+/*Entity,Code,Year,"Deaths that are from all causes attributed to smoking, in both sexes aged 70+ years",
+"Deaths that are from all causes attributed to smoking, in both sexes aged 50-69 years",
+"Deaths that are from all causes attributed to smoking, in both sexes aged 15-49 years"
+*/
+CREATE TABLE IF NOT EXISTS age_deaths(
+id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+Entity VARCHAR(20),
+Code VARCHAR(3),
+Year YEAR,
+`old` DOUBLE,
+`midage` DOUBLE,
+`young` DOUBLE
+);
+
+/*Fill a table with different fields as the data you have*/
+LOAD DATA LOCAL INFILE './data/smoking-deaths-by-age.csv'
+INTO TABLE age_deaths
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS
+(`Entity`, `Code`, `Year`,
+@csv_deaths_70, @csv_deaths_50_69, @csv_deaths_15_49)
+SET
+old=@csv_deaths_70,
+midage=@csv_deaths_50_69,
+young=@csv_deaths_15_49;
+
+
+ALTER TABLE age_deaths RENAME COLUMN `old` TO `70+`;
+ALTER TABLE age_deaths RENAME COLUMN `midage` TO `50-69`;
+ALTER TABLE age_deaths RENAME COLUMN `young` TO `15-49`;
